77fc39b56a547615fe8035a9bd3595e420064f6c
unknown <nbatre1@yahoo.com>
2019-05-12 16:51:57 -0500

    keep moving forward ,finished project

--- resorttime/src/Contentful.js
+++
@@ -1,7 +0,0 @@
-import {createClient} from 'contentful';
-
-
-export default createClient({
-    space:'96hru6g1hww1',
-    accessToken:'v8jNGpC0kPWU3o_oMt6wCig9VSseWS5LFDnzUB4k3w4'
-})
--- src/Contentful.js
+++ src/Contentful.js
@@ -1,7 +1,11 @@
-import {createClient} from 'contentful';
+const contentful = require('contentful');
 
 
-export default createClient({
-    space:'96hru6g1hww1',
-    accessToken:'v8jNGpC0kPWU3o_oMt6wCig9VSseWS5LFDnzUB4k3w4'
-})
+export default contentful.createClient({
+    // space: "48t1s0p1dk0p",
+    // accessToken:
+    //   "8bcdb00a5433f65ca9c9059c7ba3e3a7d0b3a2ccb293eb4a3c4af6951d9047a0"
+    space: '96hru6g1hww1',
+    accessToken: 'v8jNGpC0kPWU3o_oMt6wCig9VSseWS5LFDnzUB4k3w4'
+  });
+  
